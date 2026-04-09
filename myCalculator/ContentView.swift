5691b3da77e74b3d8c44866099478d93e73cbb19
渠晓友 <xiaoyouPrince@163.com>
2026-04-09 14:56:23 +0800

    Initial Commit

---
+++ myCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myCalculator
+//
+//  Created by 渠晓友 on 2026/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myCalculator/myCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCalculatorApp.swift
+//  myCalculator
+//
+//  Created by 渠晓友 on 2026/4/9.
+//
+
+import SwiftUI
+
+@main
+struct myCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
